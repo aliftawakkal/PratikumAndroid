e933823df7b234f15fd6bb25f2df1c359a709f0d
Alif Tawakkal <aliftawakkal98@gmail.com>
2020-04-24 21:04:19 +0700

    Penambahan HitungActivity, Panggil dengan Intent

--- app/src/main/java/com/example/projectkelasc/MainActivity.kt
+++ app/src/main/java/com/example/projectkelasc/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView1 = findViewById<TextView>(R.id.textView1)
-        val button1 = findViewById<Button>(R.id.button1)
+//        val textView1 = findViewById<TextView>(R.id.textView1)
+//        val button1 = findViewById<Button>(R.id.button1)
 
         textView1.setOnClickListener {
             Toast.makeText(this, "Hai",Toast.LENGTH_SHORT ).show()
