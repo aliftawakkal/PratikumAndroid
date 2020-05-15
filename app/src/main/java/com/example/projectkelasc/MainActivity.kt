18d98b7d4983080f8d650fe1245dbdc392fe85be
aliftawakkal <aliftawakkal98@gmail.com>
2020-05-15 12:55:55 +0700

    Bikin Intent Extra dan AlertDialog With EditText Pada GameActivity

--- app/src/main/java/com/example/projectkelasc/MainActivity.kt
+++ app/src/main/java/com/example/projectkelasc/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.projectkelasc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,8 +27,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         buttonGunting.setOnClickListener {
-            val intent = Intent( this@MainActivity,GameActivity::class.java)
-            startActivity(intent)
+            var alert:AlertDialog.Builder = AlertDialog.Builder(this)
+            val editUser = EditText(this)
+            alert.setTitle("Username")
+            alert.setMessage("Hai")
+            alert.setView(editUser)
+
+
+            alert.setPositiveButton("Yes"){dialog, which ->
+                var username = editUser.text.toString()
+                val intent = Intent( this@MainActivity,GameActivity::class.java)
+                intent.putExtra(GameActivity.EXTRA_USER, username)
+                startActivity(intent)
+            }
+
+            alert.setNegativeButton("No"){dialog, which ->
+
+            }
+
+            alert.show()
+
+
         }
     }
 }
